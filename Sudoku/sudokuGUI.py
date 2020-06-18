4c64834c5669b3dd1d4a885adac8a7f93688e857
Cezar Todirisca <cezartodirisca@gmail.com>
2020-06-18 20:42:25 +0300

    Final Update
    Added color scheme, random display board for the beginning(for the sole purpose of example, in many cases it is not even valid).

--- Sudoku/sudokuGUI.py
+++ Sudoku/sudokuGUI.py
@@ -4,22 +4,39 @@ import time
 import sys
 import os
 import easygui
+import random
 
 WIDTH = 600
 HEIGHT = 660
 
 # Colors
-BACK_COLOR = (17, 17, 17)
-TEXT_COLOR = (186, 197, 186)
-LINE_COLOR = (49, 156, 48)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
 
+BLUE = (0,17,255)
+GREEN = (49, 156, 48)
+PINK = (242,42,114)
+
+BACK_COLOR = (17, 17, 17)
+TEXT_COLOR = (186, 197, 186)
+LINE_COLOR = GREEN
+
 START_PRESSED = 0
 FINISH_PRESSED = 0
 
+def pickColor(x,y):
+    global LINE_COLOR
+    
+    if x>=390 and x<=420 and y>=600 and y<=630:
+        LINE_COLOR = BLUE
+    if x>420 and x<=450 and y>=600 and y<=630:
+        LINE_COLOR = GREEN
+    if x>450 and x<=480 and y>=600 and y<=630:
+        LINE_COLOR = PINK
+
 def drawLines():
+    #(0,14,217)
     # The main lines
     for i in (210, 390):
         pygame.draw.line(windowSurface, LINE_COLOR, (i, 30), (i, 570), 2)
@@ -42,9 +59,12 @@ def drawButtons():
         windowSurface.blit(finishButton, (120 + (45 - finishButton.get_width()//2),
                                     600 + (15 - finishButton.get_height()//2)))
         
+    pygame.draw.rect(windowSurface,BLUE,(390,600,30,30)) 
+    pygame.draw.rect(windowSurface,GREEN,(420,600,30,30))    
+    pygame.draw.rect(windowSurface,PINK,(450,600,30,30))       
+        
     pygame.draw.rect(windowSurface, LINE_COLOR, (210, 600, 90, 30), 1)
     pygame.draw.rect(windowSurface, LINE_COLOR, (300, 600, 90, 30), 1)
-    pygame.draw.rect(windowSurface, LINE_COLOR, (390, 600, 90, 30), 1)
 
     if START_PRESSED == 0:
         startButton = smallFont.render("Start", True, TEXT_COLOR)
@@ -56,6 +76,8 @@ def drawButtons():
     quitButton = smallFont.render("Quit", True, TEXT_COLOR)
     windowSurface.blit(quitButton, (300 + (45 - quitButton.get_width()//2),
                                     600 + (15 - quitButton.get_height()//2)))
+    
+    
 
 
 def checkEvent(board):
@@ -73,6 +95,8 @@ def checkEvent(board):
                 if mouse[0] >= 300 and mouse[0] <= 390 and mouse[1] >= 600 and mouse[1] <= 630:
                     pygame.quit()
                     sys.exit()
+                if mouse[0] >= 390 and mouse[0] <= 480 and mouse[1] >= 60 and mouse[1] <= 630:
+                    pickColor(mouse[0],mouse[1])
                 if mouse[0] >= 210 and mouse[0] <= 300 and mouse[1] >= 600 and mouse[1] <= 630:
                     if START_PRESSED == 1:
                         count += 1
@@ -83,17 +107,23 @@ def checkEvent(board):
                 if mouse[0] >= 120 and mouse[0] <= 210 and mouse[1] >= 600 and mouse[1] <= 630:
                     if START_PRESSED == 1:
                         FINISH_PRESSED = 1
+                        START_PRESSED = 0
 
         if count != 1:
             break
 
 # Display the interface.
-def redraw(board):
+def redraw(board=None):
 
     windowSurface.fill(BACK_COLOR)
     drawLines()
     drawButtons()
     gap = (WIDTH-60) // 9
+    
+    if board == None:
+        pygame.display.update
+        return
+    
     for line in range(9):
         for column in range(9):
             x = column * gap + 30
@@ -200,6 +230,16 @@ def readBoard(test=True):
     return tempBoard
 
 
+def getRandomBoard():
+    noOfNumbers = int(random.randint(15,25))
+    board = getBoard()
+    for i in range(noOfNumbers):
+        x,y = random.randint(0,8),random.randint(0,8)
+        val = random.randint(1,9)
+        board[x][y] = str(val)
+    
+    return board
+
 def mainProgram():
     board = getBoard()
     board = readBoard(test=False)
@@ -211,15 +251,15 @@ def mainProgram():
         redraw(board)
     else:
         print("This Sudoku board is not solvable")
-
+    return board
 
 def main():
     #Initial display.
     global START_PRESSED, FINISH_PRESSED
-    testBoard = getBoard()
-    testBoard = readBoard()
+    testBoard = getRandomBoard()
     redraw(testBoard)
     while True:
+        
         START_PRESSED = 0
         FINISH_PRESSED = 0 
         for event in pygame.event.get():
@@ -231,9 +271,14 @@ def main():
                 if mouse[0] >= 300 and mouse[0] <= 390 and mouse[1] >= 600 and mouse[1] <= 630:
                     pygame.quit()
                     sys.exit()
+                if mouse[0] >= 390 and mouse[0] <= 480 and mouse[1] >= 60 and mouse[1] <= 630:
+                    pickColor(mouse[0],mouse[1])
+                    redraw(testBoard)
                 if mouse[0] >= 210 and mouse[0] <= 300 and mouse[1] >= 600 and mouse[1] <= 630:
                     START_PRESSED = 1 - START_PRESSED
-                    mainProgram()
+                    testBoard = mainProgram()
+                
+                    
 
  # Initialize the window and font
 pygame.init()
